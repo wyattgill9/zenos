3af9a15525e04f8b54636723892cf2fc68d65ca2
wyattgill9 <wyattgill2009@gmail.com>
2025-05-15 12:55:00 -0700

    Beginnings

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,22 @@
-#![no_std]
-#![no_main]
+#![allow(dead_code)]
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum TaskState {
+    Ready,
+    Running,
+    Blocked,
+    Suspended,
+    Terminated,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+enum TaskPriority {
+    Low = 0,
+    Normal = 1,
+    High = 2,
+    Critical = 3,
+}
+
+type TaskId = u32;
+
+fn main() {}
